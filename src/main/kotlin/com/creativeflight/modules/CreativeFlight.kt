bb1a5662429870c7365863e87deab6835442e44f
nigerianprince2b2t <nigerian432@gmail.com>
2026-03-31 13:34:44 +0300

    fixed on disbale speed and state

--- src/main/kotlin/com/creativeflight/modules/CreativeFlight.kt
+++ src/main/kotlin/com/creativeflight/modules/CreativeFlight.kt
@@ -1,30 +1,46 @@
 package com.creativeflight.modules
 
 import com.lambda.event.events.TickEvent
+import com.lambda.event.events.PacketEvent
 import com.lambda.event.listener.SafeListener.Companion.listen
 import com.lambda.module.Module
 import com.lambda.module.tag.ModuleTag
-
+import net.minecraft.network.packet.c2s.play.UpdatePlayerAbilitiesC2SPacket
+import net.minecraft.network.packet.s2c.play.PlayerAbilitiesS2CPacket
 
 object CreativeFlight : Module(
     name = "CreativeFlight",
-    description = "Allows you to fly in survival mode.",
+    description = "Allows you to fly with custom speed.",
     tag = ModuleTag.MOVEMENT,
 ) {
     private val speed by setting("Speed", 0.1f, 0.0f..2.0f, 0.01f)
+    private var wasFlying = false
 
     init {
-        listen<TickEvent.Post> {
+        onEnable {
+            if (player.isSpectator) return@onEnable
+            wasFlying = player.abilities.flying
             player.abilities.flying = true
-            player.abilities.allowFlying = true
-            player.abilities.setFlySpeed(speed)
+            if (!player.abilities.creativeMode) player.abilities.allowFlying = true
         }
 
         onDisable {
-            if (player.abilities.creativeMode || player.isSpectator) return@onDisable
-            player.abilities.flying = false
-            player.abilities.allowFlying = false
+            if (player.isSpectator) return@onDisable
+            player.abilities.flying = wasFlying
             player.abilities.setFlySpeed(0.05f)
+            if (!player.abilities.creativeMode) player.abilities.allowFlying = false
+            connection.sendPacket(UpdatePlayerAbilitiesC2SPacket(player.abilities))
+        }
+
+        listen<TickEvent.Post> {
+            if (player.isSpectator) return@listen
+            player.abilities.flying = true
+            player.abilities.setFlySpeed(speed)
+            if (!player.abilities.creativeMode) player.abilities.allowFlying = true
+        }
+
+        listen<PacketEvent.Receive.Pre> { event ->
+            if (event.packet is PlayerAbilitiesS2CPacket) event.cancel()
         }
     }
 }
